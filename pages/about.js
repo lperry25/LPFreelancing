2daa34ac622a4ede1b94b297184bd25672062bd9
Laura Perry <lauraperry@peggys-MacBook-Air.local>
2018-10-04 13:41:19 +0200

    completed the switch from next.js to react.js setup for the app

--- pages/about.js
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/MyLayout.js'
-import Link from 'next/link'
-
-
-export default () => (
-    <Layout>
-    	<p>This is the about page</p>
-    </Layout>
-)
---
+++ src/components/HomeBanner.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import {
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselCaption
+} from 'reactstrap';
+
+import banner1 from "../static/.banner.jpg.icloud";
+import banner2 from "../static/banner2.jpg";
+import banner3 from "../static/.banner3.jpg.icloud";
+
+const items = [
+    {
+        src: "../static/banner2.jpg",
+        altText: 'LP Freelancing',
+        caption: 'Software development to meet your needs'
+    },
+ /*   {
+        src: {banner2},
+        altText: 'Slide 2',
+        caption: 'Slide 2'
+    },
+    {
+        src: {banner3},
+        altText: 'Slide 3',
+        caption: 'Slide 3'
+    }*/
+];
+
+class HomeBanner extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeIndex: 0 };
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+        this.goToIndex = this.goToIndex.bind(this);
+        this.onExiting = this.onExiting.bind(this);
+        this.onExited = this.onExited.bind(this);
+    }
+
+    onExiting() {
+        this.animating = true;
+    }
+
+    onExited() {
+        this.animating = false;
+    }
+
+    next() {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+        this.setState({ activeIndex: nextIndex });
+    }
+
+    previous() {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+        this.setState({ activeIndex: nextIndex });
+    }
+
+    goToIndex(newIndex) {
+        if (this.animating) return;
+        this.setState({ activeIndex: newIndex });
+    }
+
+    render() {
+        const { activeIndex } = this.state;
+
+        const slides = items.map((item) => {
+            return (
+                <CarouselItem
+                    onExiting={this.onExiting}
+                    onExited={this.onExited}
+                    key={item.src}
+                >
+                    <img src={banner2} alt={item.altText} width="100%"/>
+                    <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+                </CarouselItem>
+            );
+        });
+
+        return (
+            <Carousel
+                activeIndex={activeIndex}
+                next={this.next}
+                previous={this.previous}
+            >
+                <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+                {slides}
+                <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+                <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+            </Carousel>
+        );
+    }
+}
+
+
+export default HomeBanner;
